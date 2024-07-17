3b55c547455989b7f51c1624e523a82dfdf69b3c
tamzid620 <tamzid620@gmail.com>
2024-07-17 19:58:19 +0600

    banner added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/userShared/Banner/page";
+import TypeOf from "@/components/userShared/TypesOf/page";
 
 
 export default function Home() {
   return (
     <main className="mx-5">
    <Banner/>
+   <TypeOf/>
     </main>
   );
 }
---
+++ src/components/userShared/TypesOf/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TypeOf = () => {
+    return (
+        <div>
+            <h1>Type Of</h1>
+        </div>
+    );
+};
+
+export default TypeOf;
